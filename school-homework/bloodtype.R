382e227fe5d47b19c350a93fa538385b1f1dc20f
Eugene Jun <genenamu@gmail.com>
2023-10-19 10:19:48 +0900

    feat: fix and add some comments

--- school-homework/bloodtype.R
+++ school-homework/bloodtype.R
@@ -5,7 +5,7 @@ prop.table(blood)       #상대도수 분포표
 
 #막대그래프
 x11()
-par(mfrow = c(1, 2))    #그림화면에 1*2(2개)의 그래흐를 나타냄
+par(mfrow = c(1, 2))    #그림화면에 1*2(2개)의 그래프를 나타냄
 barplot(blood, col = "blue", main = "혈핵형 막대그래프")            #수직 막대 그래프
 barplot(blood, horiz = T, col = "blue", main = "혈핵형 막대그래프")   #수평 막대 그래프
 
